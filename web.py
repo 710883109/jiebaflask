09ee6f0c5f5c1b44e5bdfbe67ec8d79edbf8a776
710883109 <a410385029@gmail.com>
2021-03-07 08:07:02 +0800

    good format

--- web.py
+++ web.py
@@ -28,22 +28,14 @@ def llama():
 def sheep():
     f = request.files['csvfile']
     fstring = f.read()
-    print(fstring)
-    print(fstring.splitlines())
-    #text=[]
-    '''
-    #filename = secure_filename(file.filename)
-    #file.save('./', filename)
-    with open('filename', encoding='utf-8') as csvfile:
-# 讀取 CSV 檔案內容
-        rows = csv.reader(csvfile)
-# 以迴圈輸出每一列
-        for row in rows:
-            print(row)
-            text.append(row[0])
-    print(text)
-    seg_list = jieba.cut(text[0], cut_all=True)
-    '''
-    seg_list = jieba.cut(fstring, cut_all=True)
-    return render_template('index.html',text=seg_list)
+    #print(fstring)
+    tmp=fstring.splitlines()
+    #print(fstring.splitlines())
+    
+    text=[]
+    for sentence in tmp:
+        seg_list = jieba.cut(sentence, cut_all=True)
+        text.append("/".join(seg_list))
+
+    return render_template('index.html',text=text)
 app.run(host="0.0.0.0",port=7777,debug=True)
