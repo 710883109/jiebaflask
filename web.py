43c26f6f9da83598287d2582799fb5ee92b2ed74
710883109 <a410385029@gmail.com>
2021-03-12 14:35:58 +0800

    modify some format

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ import csv
 import jieba
 import json
 from werkzeug.utils import secure_filename
+from collections import OrderedDict
 app = Flask(__name__)
 
 @app.route("/")
@@ -25,20 +26,32 @@ def llama():
     stringtext=request.values['text']
     #print(stringtext)
     seg_list = jieba.cut(stringtext, cut_all=True)
-    print(seg_list)
-    autocut_list=[ stringtext[x]+stringtext[x+1] for x in range(len(stringtext)-1)] 
-    #print(autocut_list)
+    print("seg_list",seg_list)
+    autocut_list=[ stringtext[x:x+2] for x in range(len(stringtext)-1)] 
+    print("autocut_list1",autocut_list)
+    for i in range(len(stringtext)-2):
+        autocut_list.append(stringtext[i:i+3])
+    for i in range(len(stringtext)-3):
+        autocut_list.append(stringtext[i:i+4])
+    for i in range(len(stringtext)-4):
+        autocut_list.append(stringtext[i:i+5])
+    
+    print("autocut_list",autocut_list)
     in_list=[]
     out_list=[]
-    for item in seg_list:
+    for item in seg_list:#模組切割的詞
         if len(item)>1:
             in_list.append(item)
-    print(in_list)
+    in_list=list(dict.fromkeys(in_list))
+    print("in_list",in_list)
+
     for item in autocut_list:
         if item not in in_list:
             out_list.append(item)
-    print(out_list)
-    return render_template('sub1.html',text=in_list,t2=out_list)
+    print("out_list1",out_list)
+    out_list=list(OrderedDict.fromkeys(out_list))
+    print("out_list",out_list)
+    return render_template('sub1.html',text=in_list,t2=out_list,origin=stringtext)
 
 @app.route("/upload",methods=['POST'])
 def sheep():
